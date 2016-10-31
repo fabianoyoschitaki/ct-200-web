d15d3cf3f16b4ddfe0a7c6f48ba402ad392e7044
FABIANO SHOJI YOSCHITAKI <F0113091@portoseguro.brasil>
2016-10-31 18:05:05 -0200

    removendo arquivos desnecessarios

--- ct-200-java-master/src/br/ct200/tarefa1/processo/ProcessamentoLinguagem.java
+++
@@ -1,3 +0,0 @@
-package br.ct200.tarefa1.processo;
-
-public interface ProcessamentoLinguagem {}
--- ct-200-web-master/src/br/ct200/tarefa1/common/TipoProcessamentoEnum.java
+++
@@ -1,8 +0,0 @@
-package br.ct200.tarefa1.common;
-
-public enum TipoProcessamentoEnum {
-	UNIAO,
-	CONCATENACAO,
-	KLEENE,
-	PARENTESE;
-}
